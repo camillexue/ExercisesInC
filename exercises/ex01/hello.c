d164b1a7fe71241c798fd45c3807d21f6824de2c
camillexue <xue.camille@gmail.com>
2018-01-29 18:00:29 -0500

    Finished with hello.c part of exercise

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
-
+/*
+    In general, with optimization, the assembly code becomes much shorter
+    and more concise. It will automatically get rid of unecessary or unused
+    variables or replace functions with simpler ones.
+*/
 int main() {
-    printf("Hello, World!\n");
+    int x = 5;
+    printf("Hello, World!\n"); //optimizes by changing printf to puts
+    // printf("%d\n", x);
+
+    int y = x + 1; //optimizes by doing math first and setting it equal to y
+    printf("%d\n", y);
+
     return 0;
 }
